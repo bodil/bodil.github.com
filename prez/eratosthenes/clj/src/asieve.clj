f4165ce84fa3d9d6332959e4c92afb5c034b722c
Bodil Stokke <bodil@bodil.tv>
2011-10-11 16:29:45 +0200

    Prepared asieve for demo.

--- prez/eratosthenes/clj/src/asieve.clj
+++ prez/eratosthenes/clj/src/asieve.clj
@@ -1,12 +1,13 @@
 (ns asieve
   (:require [clojure [test :as test]]))
 
-(set! *unchecked-math* true)
+;; (set! *unchecked-math* true)
 
 (def sqr #(* %1 %1))
 
-(defn sum-primes-below [^long limit]
-    (let [^booleans numbers (make-array Boolean/TYPE (inc limit))]
+(test/with-test
+  (defn sum-primes-below [limit]
+    (let [numbers (make-array Boolean/TYPE (inc limit))]
       (aset numbers 2 true)
       (doseq [i (range 3 limit 2)] (aset numbers i true))
       (doseq [factor (range 3 (Math/sqrt limit) 2)]
@@ -17,4 +18,5 @@
                (if (aget numbers factor)
                  (+ total factor)
                  total))))
+  (test/is (= 328 (sum-primes-below 50))))
 
